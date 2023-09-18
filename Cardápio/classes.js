597f37e74f357cef27736de3dcb37bc658cc7f27
Miguel Valentini <111707132+Migvalentini@users.noreply.github.com>
2023-09-18 15:49:16 -0300

    Organization

---
+++ Cardápio/classes.js
@@ -0,0 +1,11 @@
+class Client {
+    constructor(name, email, phone, CPF, age, totalToPay, dietaryRestriction) {
+        this.name = name,
+        this.email = email,
+        this.phone = phone,
+        this.CPF = CPF,
+        this.age = age,
+        this.totalToPay = totalToPay,
+        this.c = dietaryRestriction
+    }
+}
