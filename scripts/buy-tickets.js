93ca27b1c8eda4f4fdbfb15a2bd2b73f8e09dfb2
Miguel Valentini <miguelvalentini2006@gmail.com>
2023-10-04 21:43:36 -0300

    04/10 Commit

--- scripts/buy-tickets.js
+++ scripts/buy-tickets.js
@@ -1,24 +1,33 @@
 const addAdultTicket = document.querySelector('.addAdultTicketBtn')
 const addKidTicket = document.querySelector('.addKidTicketBtn')
 
-// Obtém os botões de "Adicionar Ingresso"
 const addTicketButtons = document.querySelectorAll(".addTicketBtn");
 
-// Obtém a div onde os ingressos serão inseridos
 const ticketsContainer = document.querySelector(".tickets");
+const ticketsContainerResult = document.querySelector(".tickets-list");
 
-// Adiciona um ouvinte de evento de clique a cada botão de "Adicionar Ingresso"
+const totalValueText = document.querySelector('.total-result')
+
+let totalValue = 0
+let ticketIndex = 0
+
 addTicketButtons.forEach((button, index) => {
     button.addEventListener("click", () => {
-        // Crie um novo elemento de ingresso
         const newTicket = document.createElement("div");
         newTicket.classList.add("ticket");
+        newTicket.classList.add(`ticket${ticketIndex}`);
+        ticketIndex += 1
 
-        // Defina o conteúdo do ingresso com base no botão clicado
+        console.log(totalValue)
+
+        const newTicketResult = document.createElement("div");
+        newTicketResult.classList.add("result-preview");
+
         if (index === 0) {
-         console.log('botão adulto')
+            totalValue += 150
+            totalValueText.innerHTML = `R$${totalValue}`
+            newTicket.classList.add("ticket-adult");
             newTicket.innerHTML = `
-            <div class="ticket ticket-adult ticket0">
             <div class="ticket-texts">
                 <h3 class="ticket-type">Ingresso de Adulto</h3>
                 <div class="ticket-content ticket-name">
@@ -55,15 +64,77 @@ addTicketButtons.forEach((button, index) => {
                     </div>
                 </div>
             </div>
-        </div>   
+               
+            `;
+            newTicketResult.innerHTML = `
+                <div class="result-texts">
+                    <span class="result-type">Ingresso Adulto:</span><br>
+                    <span class="result-name">Nome: X</span><br>
+                    <span class="result-cpf">CPF: X</span><br>
+                    <span class="result-restriction">Restrição: X</span>
+                </div>
+                <div class="result-value">
+                    <span>R$150,00</span>
+                </div>
             `;
         } else if (index === 1) {
+            totalValue += 50
+            totalValueText.innerHTML = `R$${totalValue}`
+            newTicket.classList.add("ticket-kid");
             newTicket.innerHTML = `
-                <!-- Conteúdo do ingresso de Criança -->
+            <div class="ticket-texts">
+                <h3 class="ticket-type">Ingresso de Criança</h3>
+                <div class="ticket-content ticket-name">
+                    <label for="iname">Nome:</label>
+                    <input class="iname" type="text" name="iname" id="" required><br>
+                </div>
+                <div class="ticket-content ticket-cpf">
+                    <label for="icpf">CPF:</label>
+                    <input class="icpf" type="number" name="icpf" id="" required><br>
+                </div>
+                <div class="ticket-content ticket-birth">
+                    <label for="ibirth">Data de <br> Nascimento:</label>
+                    <input class="ibirth" type="date" name="ibirth" id="" required><br>
+                </div>
+                <div class="ticket-content ticket-student-teacher">
+                    <label for="itype">Aluno/Professor</label>
+                    <select class="itype" id="" name="frutas">
+                        <option value="student">Aluno</option>
+                        <option value="teacher">Professor</option>
+                    </select>
+                </div>
+            </div>
+            <div class="rest-know">
+                <button class="delete-btn">Excluir</button>
+                <div class="restriction-div">
+                    <label class="rest-label" for="irestriction">Possui Alguma Restrição Alimentar?</label>
+                    <textarea name="irestriction" id="irestriction" cols="10" rows="3" placeholder="Se sim, digite aqui..."></textarea>
+                </div>
+                <div class="know-div">
+                    <label for="know">Conhece alguém do Cetec?</label>
+                    <div class="to-know">
+                        <label class="radio-label" for="radio1"><input type="radio" id="radio1" name="know-somebody" value="yes"> Sim</label>
+                        <label class="radio-label" for="radio2"><input type="radio" id="radio2" name="know-somebody" value="no"> Não</label>
+                    </div>
+                </div>
+            </div>
+            `;
+
+            newTicketResult.innerHTML = `
+                <div class="result-texts">
+                    <span class="result-type">Ingresso Criança:</span><br>
+                    <span class="result-name">Nome: X</span><br>
+                    <span class="result-cpf">CPF: X</span><br>
+                    <span class="result-restriction">Restrição: X</span>
+                </div>
+                <div class="result-value">
+                    <span>R$50,00</span>
+                </div>
             `;
         }
 
         // Adicione o ingresso à div "tickets"
         ticketsContainer.appendChild(newTicket);
+        ticketsContainerResult.appendChild(newTicketResult)
     });
 });
---
+++ scripts/buy.js
@@ -0,0 +1,59 @@
+// Seleciona os elementos do formulário
+const nameInput = document.getElementById("iname");
+const phoneInput = document.getElementById("iphone");
+const cpfInput = document.getElementById("icpf");
+const emailInput = document.getElementById("iemail");
+
+const nameRegex = /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/;
+const phoneRegex = /^\(?\d{2}\)?[-.\s]?\d{4,5}[-.\s]?\d{4}$/;
+const cpfRegex = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
+const emailRegex = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
+
+// Função para validar o formulário
+function validarFormulario() {
+   if (nameRegex.test(nameInput.value)) {
+      nameInput.id = 'success-completing'
+   }
+
+   if (phoneRegex.test(phoneInput.value)) {
+      phoneInput.id = 'success-completing'
+   }
+
+   if (cpfRegex.test(cpfInput.value)) {
+      cpfInput.id = 'success-completing'
+   }
+
+   if (emailRegex.test(emailInput.value)) {
+      emailInput.id = 'success-completing'
+   }
+
+   if (!nameRegex.test(nameInput.value)) {
+      nameInput.id = 'unsuccess-completing'
+      return false;
+   }
+
+   if (!phoneRegex.test(phoneInput.value)) {
+      phoneInput.id = 'unsuccess-completing'
+      return false;
+   }
+
+   if (!cpfRegex.test(cpfInput.value)) {
+      cpfInput.id = 'unsuccess-completing'
+      return false;
+   }
+
+   if (!emailRegex.test(emailInput.value)) {
+      emailInput.id = 'unsuccess-completing'
+      return false;
+   }
+
+   return true;
+}
+
+// Adiciona um ouvinte de evento ao botão de envio do formulário
+const submitBtn = document.querySelector(".submitBtn");
+submitBtn.addEventListener("click", function (event) {
+   if (!validarFormulario()) {
+      event.preventDefault();
+   }
+});
