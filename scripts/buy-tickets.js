f1f85dbb05577b14e8751bbb52516ea7f0dcb4ee
Miguel Valentini <miguelvalentini2006@gmail.com>
2023-10-08 22:23:11 -0300

    08/10 Commit

--- scripts/buy-tickets.js
+++ scripts/buy-tickets.js
@@ -20,7 +20,7 @@ const totalNameText = document.querySelector('.total-name')
 const nameRegex = /^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/
 const cpfRegex = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/
 
-addTicketButtons.forEach((button, index) => {
+addTicketButtons.forEach((button) => {
     button.addEventListener("click", () => {
         const newTicket = document.createElement("div")
         newTicket.classList.add("ticket")
@@ -32,7 +32,7 @@ addTicketButtons.forEach((button, index) => {
         newTicketResult.classList.add(`result-preview${ticketIndex}`)
         newTicketResult.setAttribute("data-index", ticketIndex)
 
-        if (index === 0) {
+        if (button.classList.contains("addAdultTicketBtn")) {
             totalValue += adultPrice
             totalValueText.innerHTML = `R$${totalValue}`
             newTicket.classList.add("ticket-adult")
@@ -41,19 +41,20 @@ addTicketButtons.forEach((button, index) => {
                 <h3 class="ticket-type">Ingresso Adulto</h3>
                 <div class="ticket-content ticket-name">
                     <label for="iname">Nome:</label>
-                    <input class="iname iname${ticketIndex}" type="text" name="iname" id="" required><br>
+                    <input class="iname iname${ticketIndex}" type="text" name="iname" id="" placeholder="Nome e Sobrenome"><br>
                 </div>
                 <div class="ticket-content ticket-cpf">
                     <label for="icpf">CPF:</label>
-                    <input class="icpf icpf${ticketIndex}" type="number" name="icpf" id="" required><br>
+                    <input class="icpf icpf${ticketIndex}" type="number" name="icpf" id="" placeholder="XXX.XXX.XXX.XX"><br>
                 </div>
                 <div class="ticket-content ticket-birth">
                     <label for="ibirth">Data de <br> Nascimento:</label>
-                    <input class="ibirth ibirth${ticketIndex}" type="date" name="ibirth" id="" required><br>
+                    <input class="ibirth ibirth${ticketIndex}" type="date" name="ibirth" id=""><br>
                 </div>
                 <div class="ticket-content ticket-student-teacher">
                     <label class="itype-label itype-label${ticketIndex}" for="itype">Aluno/Professor</label>
                     <select class="itype itype${ticketIndex}" id="" data-ticket-index="${ticketIndex}">
+                        <option value="">Selecione uma opção</option>
                         <option value="student">Aluno</option>
                         <option value="teacher">Professor</option>
                     </select>
@@ -73,7 +74,6 @@ addTicketButtons.forEach((button, index) => {
                     </div>
                 </div>
             </div>
-               
             `;
             newTicketResult.innerHTML = `
                 <div class="result-texts">
@@ -86,7 +86,7 @@ addTicketButtons.forEach((button, index) => {
                     <span>R$${adultPrice},00</span>
                 </div>
             `;
-        } else if (index === 1) {
+        } else if (button.classList.contains("addKidTicketBtn")) {
             totalValue += kidPrice
             totalValueText.innerHTML = `R$${totalValue}`
             newTicket.classList.add("ticket-kid")
@@ -95,19 +95,20 @@ addTicketButtons.forEach((button, index) => {
                 <h3 class="ticket-type">Ingresso Criança</h3>
                 <div class="ticket-content ticket-name">
                     <label for="iname">Nome:</label>
-                    <input class="iname iname${ticketIndex}" type="text" name="iname" id="" required><br>
+                    <input class="iname iname${ticketIndex}" type="text" name="iname" id="" placeholder="Nome e Sobrenome"><br>
                 </div>
                 <div class="ticket-content ticket-cpf">
                     <label for="icpf">CPF:</label>
-                    <input class="icpf icpf${ticketIndex}" type="number" name="icpf" id="" required><br>
+                    <input class="icpf icpf${ticketIndex}" type="number" name="icpf" id="" placeholder="XXX.XXX.XXX.XX"><br>
                 </div>
                 <div class="ticket-content ticket-birth">
                     <label for="ibirth">Data de <br> Nascimento:</label>
-                    <input class="ibirth ibirth${ticketIndex}" type="date" name="ibirth" id="" required><br>
+                    <input class="ibirth ibirth${ticketIndex}" type="date" name="ibirth" id=""><br>
                 </div>
                 <div class="ticket-content ticket-student-teacher">
                     <label class="itype-label itype-label${ticketIndex}" for="itype">Aluno/Professor</label>
                     <select class="itype itype${ticketIndex}" id="" data-ticket-index="${ticketIndex}">
+                        <option value="">Selecione uma opção</option>
                         <option value="student">Aluno</option>
                         <option value="teacher">Professor</option>
                     </select>
@@ -151,92 +152,153 @@ addTicketButtons.forEach((button, index) => {
 ticketsContainer.addEventListener("click", (event) => {
     if (event.target.classList.contains("delete-btn")) {
         const button = event.target;
-        const ticket = button.closest(".ticket")
+        const ticket = button.closest(".ticket");
 
         if (ticket) {
-            const ticketType = ticket.classList.contains("ticket-adult") ? "adult" : "kid"
-            const ticketIndex = ticket.getAttribute("data-index")
+            const ticketType = ticket.classList.contains("ticket-adult") ? "adult" : "kid";
+            const ticketIndexToDelete = parseInt(ticket.getAttribute("data-index"));
 
             if (ticketType === "adult") {
-                totalValue -= adultPrice
+                totalValue -= adultPrice;
             } else if (ticketType === "kid") {
-                totalValue -= kidPrice
+                totalValue -= kidPrice;
             }
 
-            totalValueText.innerHTML = `R$${totalValue}`
+            totalValueText.innerHTML = `R$${totalValue}`;
 
-            ticket.remove()
-
-            const ticketResult = ticketsContainerResult.querySelector(`[data-index="${ticketIndex}"]`)
-
+            const ticketResult = ticketsContainerResult.querySelector(`.result-preview${ticketIndexToDelete}`);
             if (ticketResult) {
-                ticketResult.remove()
+                ticketResult.remove();
             }
+
+            ticket.remove();
+
+            const ticketResults = ticketsContainerResult.querySelectorAll(".result-preview");
+
+            ticketResults.forEach((ticketResult, index) => {
+                let currentIndex = parseInt(ticketResult.getAttribute("data-index"));
+
+                if (currentIndex > ticketIndexToDelete) {
+                    currentIndex -= 1;
+                    ticketResult.setAttribute("data-index", currentIndex);
+                }
+            });
         }
     }
-})
+});
+
 
 ticketsContainer.addEventListener("change", (event) => {
-    const target = event.target
+    const target = event.target;
 
     if (target.classList.contains("itype-input")) {
-        const ticketIndex = target.getAttribute("data-ticket-index")
-        const selectElement = document.querySelector(`.itype${ticketIndex}`)
-        const labelElement = document.querySelector(`.itype-label${ticketIndex}`)
+        const ticketIndex = target.getAttribute("data-ticket-index");
+        const selectElement = document.querySelector(`.itype${ticketIndex}`);
+        const labelElement = document.querySelector(`.itype-label${ticketIndex}`);
 
         if (selectElement && labelElement) {
             if (target.value === "yes") {
-                selectElement.style.display = "block"
-                labelElement.style.display = "block"
+                selectElement.style.display = "block";
+                labelElement.style.display = "block";
             } else if (target.value === "no") {
-                selectElement.style.display = "none"
-                labelElement.style.display = "none"
+                selectElement.style.display = "none";
+                labelElement.style.display = "none";
             }
         }
     }
-})
+});
 
 payBtn.addEventListener('click', () => {
     const ticketElements = document.querySelectorAll('.ticket');
+    const ticketsValues = [];
 
     if (ticketElements.length === 0) {
-        alert('Por favor, adicione pelo menos um ingresso antes de pagar.');
+        console.log('Por favor, adicione pelo menos um ingresso antes de pagar.');
         return;
     }
 
     let allTicketsValid = true;
 
     ticketElements.forEach((ticketElement, index) => {
+        console.log(index)
         const nameInput = document.querySelector(`.iname${index}`);
         const cpfInput = document.querySelector(`.icpf${index}`);
+        const birthInput = document.querySelector(`.ibirth${index}`);
+        const restrictionTextarea = document.querySelector(`.irestriction${index}`);
+        const knowInput = document.querySelector(`input[type="radio"][data-ticket-index="${index}"]:checked`);
 
         const isNameValid = nameRegex.test(nameInput.value);
         const isCPFValid = cpfRegex.test(cpfInput.value);
+        const isBirthValid = birthInput.value !== '';
 
-        // Remove todas as classes de estilo anteriores
-        nameInput.classList.remove('valid-input', 'invalid-input');
-        cpfInput.classList.remove('valid-input', 'invalid-input');
+        nameInput.value = 'Miguel Valentini'
+        cpfInput.value = '12345678910'
+        birthInput.value = '2000-01-01'
 
-        if (!isNameValid || !isCPFValid) {
+        nameInput.removeAttribute('id');
+        cpfInput.removeAttribute('id');
+        birthInput.removeAttribute('id');
+        
+        if (isNameValid) {
+            nameInput.setAttribute('id', 'success-completing');
+        }
+        if (isCPFValid) {
+            cpfInput.setAttribute('id', 'success-completing');
+        }
+        if (isBirthValid) {
+            birthInput.setAttribute('id', 'success-completing');
+        }
+        
+        if (!isNameValid || !isCPFValid || !isBirthValid) {
             allTicketsValid = false;
 
-            // Adiciona a classe "invalid-input" para destacar que o campo é inválido
             if (!isNameValid) {
-                nameInput.classList.add('invalid-input');
+                nameInput.setAttribute('id', 'unsuccess-completing');
             }
             if (!isCPFValid) {
-                cpfInput.classList.add('invalid-input');
+                cpfInput.setAttribute('id', 'unsuccess-completing');
             }
-        } else {
-            // Adiciona a classe "valid-input" para destacar que o campo é válido
-            nameInput.classList.add('valid-input');
-            cpfInput.classList.add('valid-input');
+            if (!isBirthValid) {
+                birthInput.setAttribute('id', 'unsuccess-completing');
+            }
+        }
+
+        const ticketType = ticketElement.classList.contains('ticket-adult') ? 'adult' : 'kid'; 
+
+        const selectedKnowValue = knowInput ? knowInput.value : 'não conhece ninguém'; 
+
+        const birthDate = new Date(birthInput.value);
+        const today = new Date();
+        const age = today.getFullYear() - birthDate.getFullYear();
+
+        const ticketValues = {
+            name: nameInput.value,
+            cpf: cpfInput.value,
+            birth: birthInput.value,
+            type: ticketType,
+            restriction: restrictionTextarea.value,
+            knowsSomebody: selectedKnowValue,
+            age: age
+        };
+
+        ticketsValues.push(ticketValues);
+
+        if (ticketType === 'kid') {
+            if (age >= 5 && age <= 10) {
+                console.log(`Ingresso ${index + 1} é de Criança com idade válida.`);
+            } else {
+                console.log(`Ingresso ${index + 1} é de Criança com idade inválida.`);
+                allTicketsValid = false;
+                ticketElement.setAttribute('id', 'unsuccess-completing');
+                birthInput.setAttribute('id', 'unsuccess-completing');
+            }
         }
     });
 
     if (!allTicketsValid) {
-        alert('Por favor, preencha corretamente todos os campos em vermelho.');
+        console.log('Dados inválidos');
     } else {
-        alert('Pagamento bem-sucedido!');
+        console.log('Pagamento bem-sucedido!');
+        console.log('Valores dos ingressos:', ticketsValues);
     }
 });
