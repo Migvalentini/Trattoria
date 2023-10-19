68b8a53da56102295a915196b55ce626e6ea6c59
DiegSilv <d.silveira2006@gmail.com>
2023-10-19 16:45:54 -0300

    paymant att

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,8 +1,10 @@
 const btncopiar= document.querySelector('.copiar');
-const textAreaChave=document.querySelector('.chavepix');
 
 btncopiar.addEventListener('click', (e) => {
-    navigator.clipboard.writeText(textAreaChave.value);
+    navigator.clipboard.writeText(pixkey);
     alert("Chave Pix copiada com sucesso! Acesse seu aplicativo bancario para efetuar o pagamento.")
 })
 let valorzin=220
+document.getElementById("precofinal").textContent = valorzin;
+
+pixkey="00020126330014br.gov.bcb.pix0111057200450825204000053039865802BR5923Diego Silveira Da Silva6009Sao Paulo62070503***6304E1E5"
