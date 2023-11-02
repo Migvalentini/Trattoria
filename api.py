a828e762a4b610ed24cc6f9f64ae96d8d78ee165
Migvalentini <miguelvalentini2006@gmail.com>
2023-11-02 19:07:23 -0300

    c

--- api.py
+++ api.py
@@ -3,7 +3,19 @@ import requests
 #response = requests.get('https://trattoria-three.vercel.app/create')
 #print(response.json()['json'])
 
-print('COMPRADORES\n', requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select * from Compradores;"""}).json()['json'])
-print('TOTAL COMPRADORES:', requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select count (*) from Compradores;"""}).json()['json'][0][0])
-print('INGRESSOS\n', requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select * from Ingressos;"""}).json()['json'])
-print('TOTAL INGRESSOS:', requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select count (*) from Ingressos;"""}).json()['json'][0][0])
+compradores = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select * from Compradores;"""}).json()['json']
+#totalCompradores = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select count (*) from Compradores;"""}).json()['json'][0][0]
+ingressos = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select * from Ingressos;"""}).json()['json']
+#totalIngressos = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select count (*) from Ingressos;"""}).json()['json'][0][0]
+
+somaTotal = 0
+somaPaga = 0
+for c in compradores:
+   somaTotal += int(c[6])
+   if c[6] == 'pago':
+      somaPaga += int(c[6])
+print(somaTotal)
+
+for i in ingressos:
+   if i[3] != '':
+      print(i)
