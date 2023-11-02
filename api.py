6ba3acff04f2f5b283b1687ac3eab21a649ad0ac
Miguel Valentini <miguelvalentini2006@gmail.com>
2023-11-01 22:36:02 -0300

    d

--- api.py
+++ api.py
@@ -1,11 +1,7 @@
 import requests
 
-#response = requests.post('https://trattoria-three.vercel.app/post', json={'sql':"delete from Ingressos; delete from Compradores"})
+#response = requests.get('https://trattoria-three.vercel.app/create')
 #print(response.json()['json'])
-
-# for i in range(9):
-#    response = requests.post('https://trattoria-three.vercel.app/post', json={'sql':"INSERT INTO Ingressos (nome, cpf, restricao, conhecido, tipo, id_Comprador) VALUES ('', '', '', '', '', '116') returning id"})
-#    print(response.json()['json'])
 
 print('COMPRADORES\n', requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select * from Compradores;"""}).json()['json'])
 print('TOTAL COMPRADORES:', requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select count (*) from Compradores;"""}).json()['json'][0][0])
