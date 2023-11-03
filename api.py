fdf913022bbd2c0d656d33e212d374172742f7f4
Migvalentini <miguelvalentini2006@gmail.com>
2023-11-02 22:07:20 -0300

    Update api.py

--- api.py
+++ api.py
@@ -6,16 +6,16 @@ import requests
 compradores = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select * from Compradores;"""}).json()['json']
 #totalCompradores = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select count (*) from Compradores;"""}).json()['json'][0][0]
 ingressos = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select * from Ingressos;"""}).json()['json']
-#totalIngressos = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select count (*) from Ingressos;"""}).json()['json'][0][0]
+totalIngressos = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select count (*) from Ingressos;"""}).json()['json'][0][0]
+print(totalIngressos)
+# somaTotal = 0
+# somaPaga = 0
+# for c in compradores:
+#    somaTotal += int(c[6])
+#    if c[6] == 'pago':
+#       somaPaga += int(c[6])
+# print(somaTotal)
 
-somaTotal = 0
-somaPaga = 0
-for c in compradores:
-   somaTotal += int(c[6])
-   if c[6] == 'pago':
-      somaPaga += int(c[6])
-print(somaTotal)
-
-for i in ingressos:
-   if i[3] != '':
-      print(i)
+# for i in ingressos:
+#    if i[3] != '' and i[3] != 'não' and i[3] != 'Não':
+#       print(i)
