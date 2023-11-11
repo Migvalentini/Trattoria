cb3bfb30932375b62d5d77ab4901a206bb769a5e
Miguel Valentini <miguelvalentini2006@gmail.com>
2023-11-11 19:21:24 -0300

    Update api.py

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ import requests
 
 compradores = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select * from Compradores;"""}).json()['json']
 compradores_ordem = sorted(compradores, key=lambda x: x[0])
+totalCompradores = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select count(*) from Compradores;"""}).json()['json'][0][0]
 ingressos = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select * from Ingressos;"""}).json()['json']
 ingressos_ordem = sorted(ingressos, key=lambda x: x[0])
 totalIngressos = requests.get('https://trattoria-three.vercel.app/get', json={'sql':"""select count(*) from Ingressos;"""}).json()['json'][0][0]
@@ -16,4 +17,5 @@ print('\nINGRESSOS:')
 for info in ingressos_ordem:
     print(info)
     
+print('\nTOTAL DE COMPRADORES:', totalCompradores)
 print('\nTOTAL DE INGRESSOS:', totalIngressos)
